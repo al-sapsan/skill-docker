83650c160438e5e5a16fb849f869526390e1807a
Halcón Peregrino <o.h.peregrino@mail.ru>
2024-11-21 17:19:25 +0300

    edit python file comments

--- ascii-art.py
+++ ascii-art.py
@@ -1,9 +1,8 @@
-from PIL import Image
+from PIL import Image # import the PIL library
 
-# Задаем символы ASCII, которые будут использоваться для создания арта
+# Set the ASCII characters that will be used to create the art
 ASCII_CHARS = ["@", "#", "S", "%", "?", "*", "+", ";", ":", ",", "."]
 
-# Функция для изменения размера изображения
 def resize_image(image, new_width=100):
     width, height = image.size
     aspect_ratio = height / width
@@ -11,46 +10,41 @@ def resize_image(image, new_width=100):
     resized_image = image.resize((new_width, new_height))
     return resized_image
 
-# Преобразуем изображение в оттенки серого
-def edit_grayscale_image(image):
+def modify_into_grayscale_image(image):
     return image.convert("L")
 
-# Сопоставляем каждый пиксель с символом ASCII
-def edit_pixels_to_ascii(image):
+# Map each pixel to an ASCII character
+def transform_pixels_to_ascii(image):
     pixels = image.getdata()
     ascii_str = ""
     for pixel in pixels:
         ascii_str += ASCII_CHARS[pixel // 25]
     return ascii_str
 
-# Основная функция
+# Main function
 def convert_image_to_ascii(image_path, new_width=100):
     try:
         image = Image.open(image_path)
     except Exception as e:
         print(e)
         return
-    
-    # Изменение размера и преобразование в оттенки серого
+    # Resize and convert to grayscale
     image = resize_image(image, new_width)
-    image = edit_grayscale_image(image)
-    
-    # Преобразуем пиксели в ASCII
-    ascii_str = edit_pixels_to_ascii(image)
-    
-    # Форматируем строку ASCII для правильного отображения
+    image = modify_into_grayscale_image(image)
+    # Convert pixels to ASCII
+    ascii_str = transform_pixels_to_ascii(image)
+    # Format an ASCII string for proper display
     img_width = image.width
     ascii_art = ""
     for i in range(0, len(ascii_str), img_width):
         ascii_art += ascii_str[i:i + img_width] + "\n"
-    
     return ascii_art
 
-# Пример использования
-path = "/Users/a2141/al-sapsan-git/skill-docker/drone.jpeg"
+# Example of use
+path = "~/drone.jpeg"
 ascii_art = convert_image_to_ascii(path)
 print(ascii_art)
 
-# Если вы хотите сохранить ASCII-арт в файл:
+# Saving the obtained result to a file
 with open("ascii_image.txt", "w") as f:
     f.write(ascii_art)
